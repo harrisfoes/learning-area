a6efbb18e082d0425ad4b5d48b0b09e517975e01
Harris Escalante <11287035+harrisfoes@users.noreply.github.com>
2023-10-11 10:40:46 +0900

    async-apis-react beginner

---
+++ react-crash/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import {useState} from 'react';
+
+const Person = (props) => {
+  return (
+    <> 
+    <h1>Name: {props.name}</h1>
+    <h2>Last Name: {props.lastName}</h2>
+    <h3>Age: {props.age}</h3>
+    </>
+  )
+}
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <Person name="Mimi" lastName= "Josephone" age="25" />
+      <Person name= "Joan" lastName="Miller" age="22" />
+    </div>
+  );
+}
+
+export default App;
